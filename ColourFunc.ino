c5508712b8244a89560cb905551fb5d3fae371c2
lJobot88l <johannewaldsen@gmail.com>
2022-06-19 22:06:16 +0200

    use bytes instead of text for transmission

--- ColourFunc.ino
+++
@@ -1,7 +0,0 @@
-void StaticColour(String colour, int panels[] = { 999 })
-{
-	for (int i = 0; i < sizeof(panels)/sizeof(panels[0]); i++)
-	{
-		Serial.println(sprintf("%03d COL %d", panels[i], colour));
-	}
-}
--- arduleaf.ino
+++ arduleaf.ino
@@ -2,15 +2,15 @@ bool startident = false;
 bool isMaster = true;
 
 int AReqIdent = 2; // Output to request the next modules identification (at output A)
-int AFB = 3;       // Input for ident feedback (from output A)
+int AFB = 4;       // Input for ident feedback (from output A)
 
-int BReqIdent = 4;  // See above
-int BFB = 5;
+int BReqIdent = 7;  // See above
+int BFB = 8;
 
-int VReqIdent = 6; // Input for ident request on this panel
-int VFB = 7;       // Output for ident feedback from this panel
+int VReqIdent = 12; // Input for ident request on this panel
+int VFB = 13;       // Output for ident feedback from this panel
 
-int R = 6, G = 7, B = 13; // Outputs for RGB
+int R = 3, G = 5, B = 6; // Outputs for RGB
 
 int id;             // This panels ID
 int panelcount = 1; // Number of panels in the system
@@ -22,7 +22,10 @@ void setup()
     pinMode(AFB, INPUT);
     pinMode(BReqIdent, OUTPUT);
     pinMode(BFB, INPUT);
-    if(Serial.print("Check if Master") == 0)
+    pinMode(R, OUTPUT);
+    pinMode(G, OUTPUT);
+    pinMode(B, OUTPUT);
+    if(Serial.write(0) != 0)
     {
         id = 0;
         Identnext();
@@ -33,14 +36,14 @@ void setup()
         {
             if (digitalRead(AFB) == HIGH && !hasVerifiedA) // Waits for feedback from output A
             {
-                Serial.print(printf("%03d", panelcount));
+                Serial.write(panelcount);
                 panelcount++;
                 hasVerifiedA = true;
                 s = millis();
             }
             if (digitalRead(BFB) == HIGH && !hasVerifiedB) // Waits for feedback from output B
             {
-                Serial.print(printf("%03d", panelcount));
+                Serial.write(panelcount);
                 panelcount++;
                 hasVerifiedB = true;
                 s = millis();
@@ -57,8 +60,7 @@ void setup()
 
             if(millis() - s > 1000)
             {
-                Serial.print("Timeout");
-                s = millis();
+                break;
             }
         }
     } 
@@ -81,44 +83,22 @@ void setup()
 
 void loop() 
 {
-    if(isMaster)
+    if(Serial.available())
     {
-        String out = Serial.readString();
-        char address[3];
-        sprintf(address, "%03d", id);
-        if(out.startsWith(address))
+        byte bytes[64];
+        Serial.readBytes(bytes, 64);
+        if((int)bytes[0] == id)
         {
-            out = out.substring(4);
-            if(out.substring(0, 3) == "RNB")
+            if(bytes[1] == 0x01)
             {
             }
-            else if(out.substring(0, 3) == "COL")
+            else if(bytes[1] == 0x02)
             { // Set static colour, format: ID COL 000000
 
-                //StaticColour(out.substring(4,6));
-            }
-        }
-    }
-    else
-    {
-        String out = Serial.readString();
-        char address[3];
-        sprintf(address, "%03s", out.substring(3));
-        if(out.startsWith(address) || out.startsWith("999"))
-        {   // ID  CMD DATA
-            // 001 COL 00FF00
-            out = out.substring(4); // The id is 3 digits long
-            if(out.substring(0, 3 == "COL"))
-            {
-                char Rc[2];
-                char Gc[2];
-                char Bc[2];
-                sprintf(R, "%03s", out.substring(4, 2));
-                sprintf(G, "%03s", out.substring(6, 2));
-                sprintf(B, "%03s", out.substring(8, 2));
-                analogWrite(R, strtol(Rc, 0, 16));
-                analogWrite(G, strtol(Gc, 0, 16));
-                analogWrite(B, strtol(Bc, 0, 16));
+                analogWrite(R, bytes[2]);
+                analogWrite(G, bytes[3]);
+                analogWrite(B, bytes[4]);
+                digitalWrite(13, HIGH);
             }
         }
     }
